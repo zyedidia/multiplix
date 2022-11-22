16ccf3b0fdaf28f0bb30858433221771d6f72c48
Zachary Yedidia <zyedidia@gmail.com>
2022-11-22 14:12:31 -0800

    Map devices at kernel addresses

--- kernel/board/qemuvirt/system.d
+++ kernel/board/qemuvirt/system.d
@@ -2,6 +2,7 @@ module kernel.board.qemuvirt.system;
 
 import kernel.dev.uart.ns16550;
 import kernel.dev.reboot.syscon;
+import kernel.vm;
 
-alias Reboot = SysCon!(cast(uint*) 0x100000);
-alias Uart = Ns16550!(cast(uint*) 0x10000000);
+alias Reboot = SysCon!(cast(uint*) pa2ka(0x100000));
+alias Uart = Ns16550!(cast(uint*) pa2ka(0x10000000));
--- kernel/board/visionfive/system.d
+++ kernel/board/visionfive/system.d
@@ -3,8 +3,14 @@ module kernel.board.visionfive.system;
 import kernel.dev.uart.dw8250;
 import kernel.dev.reboot.sbi;
 import kernel.dev.gpio.starfive;
-import vm = kernel.vm;
+import kernel.vm;
 
-alias Uart = Dw8250!(0x12440000);
-alias Reboot = SbiReboot;
-alias Gpio = StarfiveGpio!(cast(uint*) vm.pa2ka(0x1191_0000));
+version (baremetal) {
+    alias Uart = Dw8250!(0x12440000);
+    alias Reboot = SbiReboot;
+    alias Gpio = StarfiveGpio!(cast(uint*) 0x11910000);
+} else {
+    alias Uart = Dw8250!(pa2ka(0x12440000));
+    alias Reboot = SbiReboot;
+    alias Gpio = StarfiveGpio!(cast(uint*) pa2ka(0x11910000));
+}
