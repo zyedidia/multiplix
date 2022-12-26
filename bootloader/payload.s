103ecb7ff3549f357d7e189b77c8ec9e389ed49d
Zachary Yedidia <zyedidia@gmail.com>
2022-12-26 16:20:46 -0500

    Add space in bootloader for pagetable

--- bootloader/payload.s
+++ bootloader/payload.s
@@ -1,15 +1,13 @@
 #define XSTR(s) STR(s)
 #define STR(s) #s
 
-.section .rodata
+.section .payload
     .global payload
-    .type   payload, @object
     .align  4
 payload:
     .incbin XSTR(PAYLOAD)
 payload_end:
     .global payload_size
-    .type   payload_size, @object
     .align  4
 payload_size:
     .int    payload_end - payload
