8d2b475ce7e8aa6f25adaafeae6ab58674068a77
Zachary Yedidia <zyedidia@gmail.com>
2022-12-31 22:46:27 -0500

    raspi4 initial support

--- kernel/board/package.d
+++ kernel/board/package.d
@@ -2,6 +2,8 @@ module kernel.board;
 
 version (raspi3) {
     public import kernel.board.raspi3;
+} else version (raspi4) {
+    public import kernel.board.raspi4;
 } else version (visionfive) {
     public import kernel.board.visionfive;
 } else version (virt_riscv64) {
---
+++ kernel/board/raspi4.d
@@ -0,0 +1,26 @@
+module kernel.board.raspi4;
+
+import kernel.dev.uart.bcmmini;
+import kernel.dev.gpio.bcm;
+import kernel.dev.reboot.bcmreboot;
+
+import sys = kernel.sys;
+
+struct System {
+    enum gpu_freq = 250 * 1000 * 1000;
+
+    enum device_base = 0xfe000000;
+
+    struct MemRange {
+        uintptr start;
+        size_t sz;
+    }
+
+    enum MemRange mem = MemRange(0, sys.gb!(2));
+    /* enum MemRange mem2 = MemRange(sys.gb!(4), sys.gb!(10)); */
+    enum MemRange device = MemRange(device_base, sys.gb!(1));
+}
+
+alias Uart = BcmMiniUart!(System.device_base + 0x215000);
+alias Gpio = BcmGpio!(System.device_base + 0x200000);
+alias Reboot = BcmReboot!(System.device_base + 0x10001c, System.device_base + 0x100024);
