4d41fc3961d2b1ef79c0c046ebb31d66cca5a2dd
Zachary Yedidia <zyedidia@gmail.com>
2023-01-06 14:19:34 -0500

    Put spinlocks in bss

--- kernel/spinlock.d
+++ kernel/spinlock.d
@@ -4,13 +4,11 @@ import core.sync;
 
 // Basic mutual exclusion lock.
 struct Spinlock {
-    // We use locks to synchronize initializing the BSS, so locks should not be
-    // stored in the BSS.
-    shared uint unlocked = 1;
+    shared uint locked = 0;
 
     // Acquire the lock.
     shared void lock() {
-        while (!atomic_cmp_xchg(&unlocked, 1, 0).exchanged) {
+        while (!atomic_cmp_xchg(&locked, 0, 1).exchanged) {
         }
         memory_fence();
     }
@@ -18,6 +16,6 @@ struct Spinlock {
     // Release the lock.
     shared void unlock() {
         memory_fence();
-        atomic_store(1, &unlocked);
+        atomic_store(0, &locked);
     }
 }
