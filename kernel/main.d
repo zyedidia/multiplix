745cb8f0ca0aa0b4089a4382151d195b491865a2
Zachary Yedidia <zyedidia@gmail.com>
2023-01-07 16:54:34 -0800

    Lto not working on raspi3

--- kernel/main.d
+++ kernel/main.d
@@ -16,16 +16,17 @@ __gshared bool primary = true;
 
 extern (C) void kmain(int coreid, ubyte* heap) {
     io.writeln("entered kmain at: ", &kmain, " core: ", cpuinfo.coreid);
+    io.writeln("entered kmain at: ", &kmain, " core: ", cpuinfo.coreid);
 
-    KrAllocator kr = KrAllocator(heap, sys.mb!(128));
-    void* p = kr.alloc(10);
-    io.writeln("allocated: ", p);
-
-    arch.Trap.init();
-    arch.Trap.enable();
-    arch.Timer.intr();
-
-    while (1) {}
-
+    /* KrAllocator kr = KrAllocator(heap, sys.mb!(128)); */
+    /* void* p = kr.alloc(10); */
+    /* io.writeln("allocated: ", p); */
+    /*  */
+    /* arch.Trap.init(); */
+    /* arch.Trap.enable(); */
+    /* arch.Timer.intr(); */
+    /*  */
+    /* while (1) {} */
+    /*  */
     Reboot.reboot();
 }
