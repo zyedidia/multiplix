f657420c8f3cdff5415af2cfde52aba6c629573e
Zachary Yedidia <zyedidia@gmail.com>
2023-01-26 23:05:12 -0800

    Add simd benchmark

--- kernel/main.d
+++ kernel/main.d
@@ -20,6 +20,10 @@ shared Spinlock lock;
 
 auto hello_elf = cast(immutable ubyte[]) import("user/hello/hello.elf");
 
+extern (C) void nops();
+extern (C) void simd_store();
+extern (C) void stack_store();
+
 extern (C) void kmain(int coreid, ubyte* heap) {
     arch.Trap.setup();
 
@@ -52,13 +56,25 @@ extern (C) void kmain(int coreid, ubyte* heap) {
 
     Timer.delay_ms(100);
 
-    foreach (vamap; ptable.procs[0].pt.range()) {
-        io.writeln(Hex(vamap.va), " ", Hex(vamap.pa), " ", vamap.user, " ", vamap.size);
+    for (int i = 0; i < 10; i++) {
+        io.writeln(Timer.time_fn!(10)(() {
+            stack_store();
+        }));
     }
 
-    enable_irq();
+    for (int i = 0; i < 10; i++) {
+        io.writeln(Timer.time_fn!(10)(() {
+            simd_store();
+        }));
+    }
 
-    schedule();
+    /* foreach (vamap; ptable.procs[0].pt.range()) { */
+    /*     io.writeln(Hex(vamap.va), " ", Hex(vamap.pa), " ", vamap.user, " ", vamap.size); */
+    /* } */
+    /*  */
+    /* enable_irq(); */
+    /*  */
+    /* schedule(); */
 }
 
 void enable_irq() {
