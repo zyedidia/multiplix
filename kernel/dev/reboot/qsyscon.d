148d72e86ed8ae999766efacf8d2fc1ef4755465
Zachary Yedidia <zyedidia@gmail.com>
2023-01-31 15:47:30 -0800

    Fix noreturn

--- kernel/dev/reboot/qsyscon.d
+++ kernel/dev/reboot/qsyscon.d
@@ -5,10 +5,12 @@ import core.volatile;
 // Qemu syscon device.
 struct QemuSyscon(uintptr base) {
     static noreturn shutdown() {
-        volatile_st(cast(uint*)base, 0x5555);
+        volatile_st(cast(uint*) base, 0x5555);
+        while (1) {}
     }
 
     static noreturn reboot() {
-        volatile_st(cast(uint*)base, 0x7777);
+        volatile_st(cast(uint*) base, 0x7777);
+        while (1) {}
     }
 }
