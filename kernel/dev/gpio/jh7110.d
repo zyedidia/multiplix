62e1c11cd10f517ac326e5c79093bf7cd712a73f
Zachary Yedidia <zyedidia@gmail.com>
2023-02-19 19:26:43 -0800

    Improve jh7110 gpio driver

--- kernel/dev/gpio/jh7110.d
+++ kernel/dev/gpio/jh7110.d
@@ -6,8 +6,11 @@ import bits = ulib.bits;
 import io = ulib.io;
 
 struct Jh7110Gpio(uintptr base) {
-    enum doen = cast(uint*)(base + 0x0);
-    enum dout = cast(uint*)(base + 0x40);
+    enum doen_reg = cast(uint*)(base + 0x0);
+    enum dout_reg = cast(uint*)(base + 0x40);
+
+    enum dout_mask = 0x7f;
+    enum doen_mask = 0x3f;
 
     static void set(uint pin) {
         if (pin > 63) {
@@ -17,12 +20,12 @@ struct Jh7110Gpio(uintptr base) {
         uint offset = pin / 4;
         uint shift = 8 * (pin % 4);
 
-        uint dout_off = volatile_ld(&dout[offset]);
-        uint doen_off = volatile_ld(&doen[offset]);
+        uint dout = volatile_ld(&dout_reg[offset]);
+        uint doen = volatile_ld(&doen_reg[offset]);
 
-        volatile_st(&dout[offset], bits.write(dout_off, shift+6, shift, 1));
+        volatile_st(&dout_reg[offset], dout & ~(dout_mask << shift) | (1 << shift));
         // enable is active low
-        volatile_st(&doen[offset], bits.clear(doen_off, shift+5, shift));
+        volatile_st(&doen_reg[offset], doen & ~(doen_mask << shift));
     }
 
     static void clear(uint pin) {
@@ -33,8 +36,8 @@ struct Jh7110Gpio(uintptr base) {
         uint offset = pin / 4;
         uint shift = 8 * (pin % 4);
 
-        uint dout_off = volatile_ld(&dout[offset]);
-        volatile_st(&dout[offset], bits.write(dout_off, shift+6, shift, 0));
+        uint dout = volatile_ld(&dout_reg[offset]);
+        volatile_st(&dout_reg[offset], dout & ~(dout_mask << shift));
     }
 
     static void write(uint pin, bool value) {
