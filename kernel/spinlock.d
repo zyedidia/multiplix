2c23078604c3bc6d8822d1cff72a6888ff4bb969
Zachary Yedidia <zyedidia@gmail.com>
2023-03-01 19:30:18 -0800

    Add spinlock holding

--- kernel/spinlock.d
+++ kernel/spinlock.d
@@ -2,15 +2,18 @@ module kernel.spinlock;
 
 import core.sync;
 
+import kernel.cpu;
 import kernel.irq;
 
 // Basic mutual exclusion lock.
 struct Spinlock {
     shared uint locked = 0;
+    Cpu* cpu;
 
     // Acquire the lock.
     shared void lock() {
         Irq.push_off(); // disable interrupts to avoid deadlock
+        assert(!holding());
 
         import kernel.board;
         while (lock_test_and_set(&locked, 1) != 0) {
@@ -20,9 +23,14 @@ struct Spinlock {
 
     // Release the lock.
     shared void unlock() {
+        assert(holding());
         memory_fence();
         lock_release(&locked);
 
         Irq.pop_off();
+    }
+
+    shared bool holding() in (!Irq.is_on()) {
+        return locked && this.cpu == cpu;
     }
 }
