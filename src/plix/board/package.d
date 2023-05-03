51c6149131911c9720bd2cefbd80d7d901570c55
Zachary Yedidia <zyedidia@gmail.com>
2023-05-03 14:15:46 -0700

    Add raspi4 support

--- src/plix/board/package.d
+++ src/plix/board/package.d
@@ -4,4 +4,6 @@ version (virt_riscv64) {
     public import plix.board.virt_riscv64;
 } else version (raspi3) {
     public import plix.board.raspi3;
+} else version (raspi4) {
+    public import plix.board.raspi4;
 }
