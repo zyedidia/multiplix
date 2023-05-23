e05dd00b108490d340091c8f3adc4d9a7c34cd72
Zachary Yedidia <zyedidia@gmail.com>
2023-05-23 12:34:51 -0700

    Initial code for fs block allocator

---
+++ src/plix/fs/fs.d
@@ -0,0 +1,90 @@
+module plix.fs.fs;
+
+import plix.fs.bcache : BSIZE, Buf, bread, brelease, bwrite;
+import plix.print : println;
+
+enum {
+    FSMAGIC = 0x10203040,
+    NDIRECT = 12,
+    NINDIRECT = (BSIZE / uint.sizeof),
+    MAXFILE = NDIRECT + NINDIRECT,
+    BPB = BSIZE * 8,
+}
+
+private uint bblock(uint b, Superblock sb) {
+    return b / BPB + sb.bmapstart;
+}
+
+struct Superblock {
+    uint magic;        // Must be FSMAGIC
+    uint size;         // Size of file system image (blocks)
+    uint nblocks;      // Number of data blocks
+    uint ninodes;      // Number of inodes.
+    uint nlog;         // Number of log blocks
+    uint logstart;     // Block number of first log block
+    uint inodestart;   // Block number of first inode block
+    uint bmapstart;    // Block number of first free map block
+}
+
+struct Inode {
+    short type;              // File type
+    short major;             // Major device number (T_DEVICE only)
+    short minor;             // Minor device number (T_DEVICE only)
+    short nlink;             // Number of links to inode in file system
+    uint size;               // Size of file (bytes)
+    uint[NDIRECT+1] addrs;   // Data block addresses
+}
+
+void readsb(uint dev, Superblock* sb) {
+    import builtins : memmove;
+    Buf* bp = bread(dev, 1);
+    memmove(sb, &bp.data[0], Superblock.sizeof);
+}
+
+__gshared Superblock sb;
+
+void fsinit(uint dev) {
+    readsb(dev, &sb);
+    if (sb.magic != FSMAGIC) {
+        assert(0, "invalid file system");
+    }
+    // TODO: initlog
+}
+
+void bzero(uint dev, uint bno) {
+    import builtins : memset;
+    Buf* bp = bread(dev, bno);
+    memset(&bp.data[0], 0, BSIZE);
+    // TODO: log_write
+    brelease(bp);
+}
+
+uint balloc(uint dev) {
+    for (int b = 0; b < sb.size; b += BPB) {
+        Buf* bp = bread(dev, bblock(b, sb));
+        for (uint bi = 0; bi < BPB && b + bi < sb.size; bi++) {
+            int m = 1 << (bi % 8);
+            if ((bp.data[bi / 8] & m) == 0) {
+                bp.data[bi / 8] |= m;
+                brelease(bp);
+                bzero(dev, b + bi);
+                return b + bi;
+            }
+        }
+        brelease(bp);
+    }
+    println("balloc: out of blocks");
+    return 0;
+}
+
+void bfree(uint dev, uint b) {
+    Buf* bp = bread(dev, bblock(b, sb));
+    int bi = b % BPB;
+    int m = 1 << (bi % 8);
+    if ((bp.data[bi / 8] & m) == 0) {
+        assert(0, "freeing free block");
+    }
+    bp.data[bi / 8] &= ~m;
+    // TODO: log_write
+    brelease(bp);
+}
